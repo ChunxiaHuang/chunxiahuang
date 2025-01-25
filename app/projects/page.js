c4455df9c8a1e5230b31722362d092e39f27145f
Chunxia Huang <chunxia.huang@edu.sait.ca>
2025-01-25 00:53:52 -0700

    update project page layout

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,6 +1,18 @@
 const projects = [
-  { id: 1, title: 'E-Commerce Website', description: 'An online store with tracking...', link: 'https://github.com/...' },
-  { id: 2, title: 'Blog Platform', description: 'A dynamic blog built with Next.js...', link: 'https://demo...' },
+  {
+    id: "DGH", title: 'E-Commerce Website', description: `An online store for our clients to expand their customer 
+  base beyond physical stores. In addition, we implemented features for 
+  managing orders, products, and sales reports. To enhance efficiency, 
+  we also created a mobile app that allows clients to scan and manage 
+  products and orders.`, link: 'https://github.com/...'
+  },
+  {
+    id: "EF", title: 'Empowered Futures', description: `Id incididunt sunt eu labore mollit do qui ea. 
+  Commodo amet proident deserunt duis cupidatat officia. Magna mollit nostrud ad ex. Nostrud velit ut ea
+   officia ullamco exercitation occaecat. Culpa proident eiusmod Lorem enim velit proident minim sunt id 
+   aute eiusmod. 
+  Nostrud culpa culpa sit cupidatat anim excepteur eu cillum.`, link: 'https://demo...'
+  },
 ];
 
 export default function Projects() {
@@ -10,10 +22,15 @@ export default function Projects() {
         {projects.map((project, index) => (
           <div
             key={index}
-            className="bg-white shadow-md rounded-lg p-4 hover:shadow-lg transition">
-            <h3 className="font-bold text-xl">{project.title}</h3>
-            <p>{project.description}</p>
-            <a href={project.link} target="_blank" className="text-blue-500">View Project</a>
+            className="bg-white shadow-md rounded-lg p-4 md:p-6 hover:shadow-lg transition">
+            <h3 className="font-bold text-2xl py-2">{project.title}</h3>
+            <p className="text-gray-600">{project.description}</p>
+            <img
+              src={`/images/${project.id}/01.jpg`}
+              alt={project.id}
+              className="w-[300px] md:w-[500px] object-cover my-4"
+            />
+            <a href={project.link} target="_blank" className="text-gray-500">View Project</a>
           </div>
         ))}
       </div>
